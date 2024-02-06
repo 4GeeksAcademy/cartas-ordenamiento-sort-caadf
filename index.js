3207ce156e2660b97654750bf69913fd9af92e30
Catalina Arroyo <151681207+caadf@users.noreply.github.com>
2024-02-06 02:40:02 +0000

    mitad del ejercicio

---
+++ index.js
@@ -0,0 +1,67 @@
+let numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+let iconos = ["♦", "♥", "♠", "♣"]
+let cartas = []
+
+let draw = document.querySelector('#draw')
+let sort = document.querySelector('#sort')
+let contenedor = document.querySelector('.contenedor')
+let cartasOrdenadas = document.querySelector ('.cartasOrdenadas')
+
+draw.addEventListener('click', generarCartas)
+sort.addEventListener('click', ordenarcartas)
+
+
+function generaricono() {
+  let numero = Math.floor(Math.random() * iconos.length);
+  return numero;
+}
+
+function generarnumero() {
+  let numero = Math.floor(Math.random() * numeros.length);
+  return numero;
+}
+
+
+function generarCartas() {
+  let numeroCartas = parseInt(document.getElementById('amountcards').value);
+
+  contenedor.innerHTML = "";
+  cartas = [] 
+
+  for (let i = 0; i < numeroCartas; i++) {
+    let icono = iconos[generaricono()]
+    let num = numeros[generarnumero()]
+    let carta = { icono: icono, num: num}
+    cartas.push(carta)
+    let card = document.createElement('div')
+    card.classList.add('card')
+
+    let iconoarriba = document.createElement('div')
+    iconoarriba.classList.add('icono-arriba')
+    iconoarriba.innerHTML = icono
+
+    let numero = document.createElement('div')
+    numero.classList.add('numero')
+    numero.innerHTML = num
+
+    let iconoabajo = document.createElement('div')
+    iconoabajo.classList.add('icono-abajo')
+    iconoabajo.innerHTML = icono
+
+
+    contenedor.appendChild(card)
+    card.appendChild(iconoarriba)
+    card.appendChild(numero)
+    card.appendChild(iconoabajo)
+
+    if (icono == "♦" || icono == "♥") {
+      iconoarriba.style.color = "red";
+      iconoabajo.style.color = "red";
+    } else {
+      iconoarriba.style.color = "black";
+      iconoabajo.style.color = "black";
+    }
+
+  }
+  console.log(cartas)
+}
